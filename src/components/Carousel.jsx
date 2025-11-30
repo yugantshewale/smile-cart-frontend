a53907598741e72b11402e21592ab32bd0a82600
Yugant Shewale <yugantshewale7@gmail.com>
2025-11-30 17:48:49 +0530

    Carousels added

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+import { Left, Right } from "neetoicons";
+import { Button } from "neetoui";
+
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handleNext = () => {
+    const nextIndex = (currentIndex + 1) % imageUrls.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePrevious = () => {
+    const previousIndex =
+      (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+    setCurrentIndex(previousIndex);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex items-center">
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Left}
+          style="text"
+          onClick={handlePrevious}
+        />
+        <img
+          alt={title}
+          className="max-w-56 h-56 max-h-56 w-56"
+          src={imageUrls[currentIndex]}
+        />
+        <Button
+          className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+          icon={Right}
+          style="text"
+          onClick={handleNext}
+        />
+      </div>
+      <div className="flex space-x-1 py-1">
+        {imageUrls.map((_, index) => {
+          const defaultClasses =
+            "neeto-ui-border-black neeto-ui-rounded-full h-3 w-3 cursor-pointer border";
+
+          const dotClassNames =
+            index === currentIndex
+              ? defaultClasses.concat(" neeto-ui-bg-black")
+              : defaultClasses;
+
+          return (
+            <span
+              className={dotClassNames}
+              key={index}
+              onClick={() => setCurrentIndex(index)}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/components/constants.js
@@ -0,0 +1,6 @@
+export const IMAGE_URLS = [
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/thumbnail_61_7PaLfb.jpg",
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/1_yuRfcETI5.jpg",
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/2_HoKD2OblW.png",
+  "https://ik.imagekit.io/d9mvewbju/SmileCart/3_-6sHqwicC.png",
+];
