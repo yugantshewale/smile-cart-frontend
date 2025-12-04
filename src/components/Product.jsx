b9744658218330e334c90b30ff60b9a54bcc19ef
Yugant Shewale <yugantshewale7@gmail.com>
2025-12-04 13:57:06 +0530

    Integrated API to display product details

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,33 +1,83 @@
+import { useEffect, useState } from "react";
+
 import { Typography } from "@bigbinary/neetoui";
+import axios from "axios";
+import { Spinner } from "neetoui";
+import { append } from "ramda";
 
 import Carousel from "./Carousel";
-import { IMAGE_URLS } from "./constants";
 
-const Product = () => (
-  <div className="px-6 pb-6">
-    <div>
-      <Typography className="py-2" style="h1">
-        Infinix INBOOK
-      </Typography>
-      <hr className="border-2 border-black" />
-    </div>
-    <div className="mt-6 flex gap-4">
-      <div className="w-2/5">
-        <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
+const Product = () => {
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
+      );
+      setProduct(response.data);
+    } catch (error) {
+      console.log("An error occurred:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const {
+    name,
+    description,
+    mrp,
+    offer_price: offerPrice,
+    image_urls: imageUrls,
+    image_url: imageUrl,
+  } = product;
+  const totalDiscount = mrp - offerPrice;
+  const discountPercentage = ((totalDiscount / mrp) * 100).toFixed(1);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner />
       </div>
-      <div className="w-3/5 space-y-4">
-        <Typography style="body1">
-          Infinix Inbook X1 Ci3 10th 8GB 256GB 14 Win10 Grey - 1 Year Warranty.
+    );
+  }
+
+  return (
+    <div className="px-6 pb-6">
+      <div>
+        <Typography className="py-2" style="h1">
+          {name}
         </Typography>
-        <Typography style="body1">MRP: $395.97</Typography>
-        <Typography style="body1" weight="semibold">
-          Offer price: $374.43
-        </Typography>
-        <Typography className="text-green-600" style="body1" weight="semibold">
-          6% off
-        </Typography>
+        <hr className="border-2 border-black" />
+      </div>
+      <div className="mt-16 flex gap-4">
+        <div className="w-2/5">
+          <Carousel
+            imageUrls={append(imageUrl, imageUrls)}
+            title="Infinix Inbook"
+          />
+        </div>
+        <div className="w-3/5 space-y-4">
+          <Typography style="body1">{description}</Typography>
+          <Typography style="body1">MRP: {mrp}</Typography>
+          <Typography style="body1" weight="semibold">
+            Offer price: {offerPrice}
+          </Typography>
+          <Typography
+            className="text-green-600"
+            style="body1"
+            weight="semibold"
+          >
+            {discountPercentage}% off
+          </Typography>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default Product;
