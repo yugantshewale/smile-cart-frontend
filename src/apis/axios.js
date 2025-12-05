61b96925bf24663e04d944d4f7981860e9069971
Yugant Shewale <yugantshewale7@gmail.com>
2025-12-05 13:12:07 +0530

    Implemented Axios interceptors to set http headers and to transform response

---
+++ src/apis/axios.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { keysToCamelCase } from "neetocist";
+
+const transformResponseKeysToCamelCase = response => {
+  if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+const responseInterceptors = () => {
+  axios.interceptors.response.use(response => {
+    transformResponseKeysToCamelCase(response);
+
+    return response.data;
+  });
+};
+
+const setHttpHeaders = () => {
+  axios.defaults.headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+};
+
+export default function initializeAxios() {
+  axios.defaults.baseURL =
+    "https://smile-cart-backend-staging.neetodeployapp.com/";
+  setHttpHeaders();
+  responseInterceptors();
+}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -13,8 +13,8 @@ const Product = () => {
 
   const fetchProduct = async () => {
     try {
-      const response = await productsApi.show();
-      setProduct(response.data);
+      const product = await productsApi.show();
+      setProduct(product);
     } catch (error) {
       console.log("An error occurred:", error);
     } finally {
@@ -26,14 +26,7 @@ const Product = () => {
     fetchProduct();
   }, []);
 
-  const {
-    name,
-    description,
-    mrp,
-    offer_price: offerPrice,
-    image_urls: imageUrls,
-    image_url: imageUrl,
-  } = product;
+  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
   const totalDiscount = mrp - offerPrice;
   const discountPercentage = ((totalDiscount / mrp) * 100).toFixed(1);
 
